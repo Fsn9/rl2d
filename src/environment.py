440f6409332c4a95c25c82c86241d752977a22d9
francisco <fsn9.astro@gmail.com>
2022-05-23 22:32:00 +0100

    Major changes
    Environment simplified.
    Kinematics of agents added.
    Algorithm already converging!
    Algorithm faster, also.

--- src/environment.py
+++ src/environment.py
@@ -1,9 +1,84 @@
 from random import randint
-from qlearning import Actions
 from enum import Enum
 import numpy as np
+from math import atan2, pi, cos, sin
+from itertools import product
+
+PI = round(pi,2)
+PI_2 = round(pi * 0.5, 2)
+TWO_PI = round(2 * pi, 2)
+
+class StateSimple:
+	def __init__(self):
+		self.__azimuth = 0.0
+
+	def __call__(self, obs):
+		#print('obs: ', obs)
+		gx_ag, gy_ag = obs[3] - obs[0], obs[4] - obs[1]
+		cos_rot, sin_rot = round(cos(obs[2]),2), round(sin(obs[2]),2)
+		self.__azimuth = round(atan2(-sin_rot * gx_ag + cos_rot * gy_ag,
+			cos_rot * gx_ag + sin_rot * gy_ag), 2)
+		self.__azimuth = PI if self.__azimuth == -PI else self.__azimuth
+		return self.__azimuth
+
+	def get(self):
+		return self.__azimuth
+
+	def set(self, azimuth):
+		self.__azimuth = azimuth
+
+class StateComplex:
+	pass
+
+class Actions(Enum):
+	ROT_LEFT = -1
+	FWD = 0
+	ROT_RIGHT = 1
+
+class DiscreteSpace:
+	def __init__(self):
+		self._space = []
+	def __call__(self):
+		return self._space
+	def __add__(self, other):
+		temp = DiscreteSpace()
+		temp.space = self.space + other.space
+		return temp
+	@property
+	def space(self):
+		return self._space
+	@space.setter
+	def space(self, space):
+		self._space = space
+
+class DiscreteAngularSpace(DiscreteSpace):
+	def __init__(self, width, height):
+		super().__init__()
+		self.__width, self.__height = width, height
+		self.__generate_space()
+	def __generate_space(self):
+		for y in range(self.__height):
+			for x in range(self.__width):
+				self._space.append(round(atan2(y,x), 2))
+				self._space.append(round(atan2(-y,x), 2))
+				self._space.append(round(atan2(-y,-x), 2))
+				self._space.append(round(atan2(y,-x), 2))
+		self._space = list(dict.fromkeys(self._space))
+		self._space.sort()
+
+class DiscreteLineOfSightSpace(DiscreteSpace):
+	def __init__(self, range, shape):
+		super().__init__()
+		self.__range, self.__shape = range, shape
+		self.__generate_space()
+	def __generate_space(self):
+		if self.__shape == '+':
+			self._space = [cell for cell in product([ent.value for ent in Entities], repeat = 4 * self.__range)]
+		else: # shape == '*'
+			self._space = [cell for cell in product([ent.value for ent in Entities], repeat = 6 * self.__range)]
 
 class Entities(Enum):
+	VOID = -2
 	OBSTACLE = -1
 	EMPTY = 0
 	AGENT = 1
@@ -11,60 +86,273 @@ class Entities(Enum):
 
 class EnvEntity:
 	def __init__(self):
-		self.__pos = [0,0]
+		self._pos = [0,0]
+
+	def set_x(self, x):
+		self._pos[0] = x
+
+	def set_y(self, y):
+		self._pos[1] = y
+
+	def go_left(self):
+		self._pos[0] -= 1
+
+	def go_up(self):
+		self._pos[1] += 1
+
+	def go_right(self):
+		self._pos[0] += 1
+
+	def go_down(self):
+		self._pos[1] -= 1
 
 	@property
 	def pos(self):
-		return self.__pos
-
-	def set_x(self, x):
-		self.__pos[0] = x
-
-	def set_y(self, y):
-		self.__pos[1] = y
-
-	def go_left(self):
-		self.__pos[0] -= 1
-
-	def go_up(self):
-		self.__pos[1] -= 1
-
-	def go_right(self):
-		self.__pos[0] += 1
-
-	def go_down(self):
-		self.__pos[1] += 1
+		return self._pos
 
 	@property
 	def x(self):
-		return self.__pos[0]
+		return self._pos[0]
 
 	@property
 	def y(self):
-		return self.__pos[1]
+		return self._pos[1]
 		
-	def set_pos(self, x, y):
-		self.__pos[0], self.__pos[1] = x, y
+	def move(self, x, y):
+		self._pos[0], self._pos[1] = x, y
 
-class RewardFunction:
+class EnvAgent(EnvEntity):
 	def __init__(self):
-		pass
-	def __call__(self, event = None):
-		if event is None:
-			return 0
+		super().__init__()
+		self.__theta = 0
+
+	@staticmethod
+	def normalize_angle(ang):
+		ang = ((ang % TWO_PI) + TWO_PI) % TWO_PI
+		if ang > PI:
+			ang -= TWO_PI
+		return ang
+
+	def kinematics(self, action):
+		if action == Actions.FWD:
+			x, y = self._pos[0] + cos(self.__theta), self._pos[1] + sin(self.__theta)
+			if x < 0 or y < 0:
+				return self._pos[0], self._pos[1], self.__theta
+			return int(round(self._pos[0] + cos(self.__theta))), int(round(self._pos[1] + sin(self.__theta))), self.__theta
+		elif action == Actions.ROT_LEFT:
+			return int(self._pos[0]), int(self._pos[1]), round(self.normalize_angle(self.__theta - PI_2), 2)
+		elif action == Actions.ROT_RIGHT:
+			return int(self._pos[0]), int(self._pos[1]), round(self.normalize_angle(self.__theta + PI_2), 2)
+		else:
+			raise Exception('Not a valid action')
+
+	def move(self, x, y, ang):
+		self._pos[0], self._pos[1], self.__theta = x, y, ang
+		
+	@property
+	def theta(self):
+		return self.__theta
+	
+class RewardFunction:
+	GOAL_PRIZE = 1
+	COLLISION_PENALTY = -1
+	UNDEFINED = 0
+
+	def __init__(self, state_space):
+		self.__state_space = state_space.space
+		print('State space: ', self.__state_space)
+		self.__max_angle_variation = round(abs(atan2(1, -1)),2)
+		self.__den = 2 * self.__max_angle_variation
+		self.__inv_den = 1.0 / self.__den
+
+	def __call__(self, cur_state, action, next_state, event = None):
+		#if cur_state is None or next_state is None:
+		#	return self.UNDEFINED
+		#if event is Entities.VOID.value:
+		#	return self.UNDEFINED
+
+		if next_state == 0.0 or event == Entities.GOAL.value:
+			print('GOAL')
+			return self.GOAL_PRIZE
+
+		elif event is None:
+			return self.normalize(-self.__max_angle_variation, 
+				self.__max_angle_variation, 
+				abs(cur_state) - abs(next_state)
+			)
+
 		elif event == Entities.OBSTACLE.value:
 			print('COLLIDED')
-			return -1
-		else:
-			print('GOAL')
-			return 1
+			return self.COLLISION_PENALTY
 
-class Environment:
+		else:
+			return self.UNDEFINED
+
+	# normalize between [-1, 1]
+	def normalize(self, min, max, val):
+		return 2 * (val - min)  * self.__inv_den - 1
+
+class EmptyEnvironment:
+	def __init__(self, w, h):
+		self.__w = w
+		self.__h = h
+		self.__arena = np.zeros((self.__w,self.__h))
+		self.__state_space = DiscreteAngularSpace(self.__w, self.__h)
+		self.__agent = EnvAgent()
+		self.__goal = EnvEntity()
+		self.__entities = {
+			Entities.AGENT: self.__agent,
+			Entities.GOAL: self.__goal
+		}
+		self.__agent_state = StateSimple()
+		self.__reward_function = RewardFunction(self.__state_space)
+		self.reset()
+		self.__angles = []
+
+	def __repr__(self):
+		return str(self.__arena)
+
+	@property
+	def w(self):
+		return self.__w
+
+	@property
+	def h(self):
+		return self.__h
+
+	def reset(self, event = None):
+		for row in range(self.__h):
+			self.__arena[row, :] = [Entities.EMPTY.value]
+		self.__place_entity_random(Entities.AGENT)
+		self.__place_entity_random(Entities.GOAL)
+		return self.observe(), False, RewardFunction.UNDEFINED, Entities.VOID.value
+
+	def __place_entity_random(self, ent):
+		while True:
+			r, c = randint(0, self.__w - 1), randint(0, self.__h - 1)
+			if self.__arena[r,c] == Entities.EMPTY.value:
+				self.__arena[r,c] = ent.value
+				if ent == Entities.AGENT:
+					self.__agent.move(c, r, 0.0)
+					break
+				elif ent == Entities.GOAL:
+					self.__goal.move(c, r)
+					break
+
+	# When invalid action, skip to next iteration
+	def skip(self, last_state):
+		return last_state, False, RewardFunction.UNDEFINED, Entities.VOID.value
+
+	def step(self, action):
+		print('Decision: ', action)
+		# Clean old agent info
+		self.__arena[self.__agent.y,self.__agent.x] = Entities.EMPTY.value
+		
+		# Save last state
+		last_state = self.observe()
+		"""
+		if action == Actions.ROT_LEFT:
+			self.__agent.go(action)
+			n_x = self.__agent.x + self.__direction_map[self.__agent.theta][0]
+			n_y = self.__agent.y + self.__direction_map[self.__agent.theta][1]
+			if n_x < 0 or n_x == self.__w or n_y < 0 or n_y == self.__h:
+				neighbour = None
+			else:
+				neighbour = self.__arena[n_y, n_x]
+			next_state = self.observe()
+			if neighbour == Entities.GOAL.value:
+				return next_state, True, self.__reward_function(last_state, action, next_state, neighbour), neighbour
+	
+		elif action == Actions.FWD:
+			n_x = self.__agent.x + self.__direction_map[self.__agent.theta][0]
+			n_y = self.__agent.y + self.__direction_map[self.__agent.theta][1]
+			if n_x < 0 or n_x == self.__w or n_y < 0 or n_y == self.__h:
+				neighbour = Entities.VOID
+				return self.skip(last_state)
+			else:
+				neighbour = self.__arena[n_y, n_x]
+
+			self.__agent.move(n_x, n_y)
+			next_state = self.observe()
+			if neighbour == Entities.GOAL.value:
+				return next_state, True, self.__reward_function(last_state, action, next_state, neighbour), neighbour
+		"""
+
+		"""
+		elif action == Actions.ROT_RIGHT:
+			self.__agent.go(action)
+			n_x = self.__agent.x + self.__direction_map[self.__agent.theta][0]
+			n_y = self.__agent.y + self.__direction_map[self.__agent.theta][1]
+			if n_x < 0 or n_x == self.__w or n_y < 0 or n_y == self.__h:
+				neighbour = None
+			else:
+				neighbour = self.__arena[n_y, n_x]
+			next_state = self.observe()
+			if neighbour == Entities.GOAL.value:
+				return next_state, True, self.__reward_function(last_state, action, next_state, neighbour), neighbour
+		"""
+		# Predict next pose
+		x, y, ang = self.__agent.kinematics(action)
+		#print('next pose: ', x, y, ang)
+		if action == Actions.FWD:
+			# If next pose is out of bounds, return and skip to next decision
+			if x < 0 or x == self.__w or y < 0 or y == self.__h:
+				#print('\tskip()')
+				return self.skip(last_state)
+
+			# Get neighbour of next pose
+			neighbour = self.__arena[y,x]
+
+			# Move to predicted pose
+			self.__agent.move(x, y, ang)
+
+			# Observe next state
+			next_state = self.observe()
+
+			# If neighbour is the goal, finish episode
+			if neighbour == Entities.GOAL.value:
+				#print('\tGot goal')
+				return next_state, True, self.__reward_function(last_state, action, next_state, neighbour), neighbour
+		# action == ROT_LEFT or ROT_RIGHT
+		else:
+			# If next pose is out of bounds, neighbour is None
+			if x < 0 or x == self.__w or y < 0 or y == self.__h:
+				neighbour = None
+			else:
+				neighbour = self.__arena[y,x]
+
+			# Move to predicted pose
+			self.__agent.move(x, y, ang)
+
+			# Observe next state
+			next_state = self.observe()
+
+		self.__arena[self.__agent.y, self.__agent.x] = Entities.AGENT.value
+
+		# Observe next state
+		next_state = self.observe()
+		#print('Normal step()')
+		return next_state, False, self.__reward_function(last_state, action, next_state, None), neighbour
+
+	def observe(self):
+		return self.__agent_state(
+			(	
+				self.__agent.x, 
+				self.__agent.y, 
+				self.__agent.theta,
+				self.__goal.x, 
+				self.__goal.y
+			)
+		)
+
+class ObstacleEnvironment:
 	def __init__(self, w, h, num_obstacles):
 		self.__w = w
 		self.__h = h
 		if num_obstacles >= w * h:
 			raise ValueError('Number of obstacles needs to be lower than width * height')
+		self.__state_space = DiscreteAngularSpace(self.__w, self.__h)
+		# juntar os dois state spaces
 		self.__num_obstacles = num_obstacles
 		self.__arena = np.zeros((self.__w+2,self.__h+2))
 		self.__arena[:,0] = Entities.OBSTACLE.value
@@ -83,7 +371,7 @@ class Environment:
 			Entities.OBSTACLE: self.__obstacles,
 			Entities.GOAL: self.__goal
 		}
-		self.__reward_function = RewardFunction()
+		self.__reward_function = RewardFunction(self.__state_space)
 		self.reset()
 
 	def __repr__(self):
@@ -95,7 +383,7 @@ class Environment:
 		self.__generate_obstacles(self.__num_obstacles)
 		self.__place_entity_random(Entities.AGENT)
 		self.__place_entity_random(Entities.GOAL)
-		return self.observe(), True, self.__reward_function(event)
+		return self.observe(), True, self.__reward_function(None, None, None, event), event
 
 	def __generate_obstacles(self, num):
 		for i in range(num):
@@ -103,7 +391,7 @@ class Environment:
 				r, c = randint(1, self.__w), randint(1, self.__h)
 				if self.__arena[r,c] == Entities.EMPTY.value:
 					self.__arena[r,c] = Entities.OBSTACLE.value
-					self.__entities[Entities.OBSTACLE][i].set_pos(c, r)
+					self.__entities[Entities.OBSTACLE][i].move(c, r)
 					break
 
 	def __place_entity_random(self, ent):
@@ -112,15 +400,19 @@ class Environment:
 			if self.__arena[r,c] == Entities.EMPTY.value:
 				if ent == Entities.AGENT:
 					self.__arena[r,c] = Entities.AGENT.value
-					self.__agent.set_pos(c, r)
+					self.__agent.move(c, r)
 					break
 				elif ent == Entities.GOAL:
 					self.__arena[r,c] = Entities.GOAL.value
-					self.__goal.set_pos(c, r)
+					self.__goal.move(c, r)
 					break
 
 	def step(self, action):
+		# Clean old agent info
 		self.__arena[self.__agent.y,self.__agent.x] = Entities.EMPTY.value
+		
+		# Save last observation to feed reward function
+		last_obs = self.observe()
 
 		if action == Actions.LEFT:
 			neighbour = self.__arena[self.__agent.y, self.__agent.x - 1]
@@ -151,7 +443,9 @@ class Environment:
 				self.__agent.go_down()
 				
 		self.__arena[self.__agent.y, self.__agent.x] = Entities.AGENT.value
-		return self.observe(), False, self.__reward_function()
+
+		next_obs = self.observe()
+		return next_obs, False, self.__reward_function(last_obs, action, next_obs, None), neighbour
 
 	def __generate_los(self):
 		# [left, up, right, down]
--- src/qlearning.py
+++ src/qlearning.py
@@ -1,59 +1,18 @@
 from enum import Enum
 from math import atan2
-from xml.dom import NotFoundErr
 from numpy import linspace, pi
 from random import random, choice
-
-class StateSimple:
-	def __init__(self):
-		self.__azimuth = 0.0
-	def __call__(self, obs):
-		self.__azimuth = atan2(obs[3] - obs[1], obs[2] - obs[0])
-		return self.__azimuth
-	def get(self):
-		return self.__azimuth
-
-class StateComplex:
-	pass
-
-class Actions(Enum):
-	LEFT = 0
-	UP = 1
-	RIGHT = 2
-	DOWN = 3
-class ActionsComplex(Enum):
-	LEFT = 0
-	LEFT_UP = 1
-	UP = 2
-	UP_RIGHT = 3
-	RIGHT = 4
-	RIGHT_DOWN = 5
-	DOWN = 6
-	DOWN_LEFT = 7
-
-class DiscreteAngularSpace:
-	def __init__(self, width, height):
-		self.__width = width
-		self.__height = height
-		self.__space = []
-		self.__generate_space()
-	def __call__(self):
-		return self.__space
-	def __generate_space(self):
-		for y in range(self.__height):
-			for x in range(self.__width):
-				self.__space.append(atan2(y,x))
-				self.__space.append(atan2(-y,x))
-				self.__space.append(atan2(-y,-x))
-				self.__space.append(atan2(y,-x))
-		self.__space = list(dict.fromkeys(self.__space))
-		self.__space.sort()
+from environment import Actions, DiscreteAngularSpace, DiscreteLineOfSightSpace, StateSimple, StateComplex, Entities
 
 class StateAction:
-	def __init__(self, state, action):
+	def __init__(self, state, action, initial_alpha, gamma):
 		self.__state = state
 		self.__action = action
 		self.__q = 0
+		self.__visited = False
+		self.__initial_alpha = initial_alpha
+		self.__gamma = gamma
+		self.__num_visits = 0
 	def __repr__(self):
 		return "("+str(self.__state)+","+str(self.__action)+")\n"
 	def __str__(self):
@@ -69,45 +28,64 @@ class StateAction:
 	@property
 	def q(self):
 		return self.__q
-	@q.setter
-	def q(self, val):
-		self.__q = val
+	@property
+	def visited(self):
+		return self.__visited
+	@property
+	def num_visits(self):
+		return self.__num_visits
+	def update_q(self, reward, max_q, terminal):
+		self.__num_visits += 1
+		if not self.__visited:
+			self.__visited = True
+		if terminal:
+			self.__q = self.__q + self.__initial_alpha / self.__num_visits * (reward - self.__q)
+		else:
+			self.__q = self.__q + self.__initial_alpha / self.__num_visits * (reward + self.__gamma * max_q - self.__q)
 
 class QTable:
-	def __init__(self):
+	def __init__(self, env_width, env_height, alpha, gamma):
 		self.__table = []
-		angular_resolution_2d = 9
-		self.__state_space = DiscreteAngularSpace(5,5)
+		self.__alpha = alpha
+		self.__gamma = gamma
+		self.__state_space = DiscreteAngularSpace(env_width, env_height)
 		for state in self.__state_space():
 			for action in Actions:
-				self.__table.append(StateAction(state, action))
+				self.__table.append(StateAction(state, action, self.__alpha, self.__gamma))
 
 	def __repr__(self):
 		repr_ = ""
 		for sa in self.__table:
 			repr_ += str(sa)
 		return repr_
+	
+	@property
+	def table(self):
+		return self.__table
 
 	def get_greedy_action(self, state):
 		best_q = -10000
-		action = []
+		action = None
 		for sa in self.__table:
 			if sa.state == state:
 				if sa.q > best_q:
 					best_q = sa.q
 					action = sa.action
+		if action is None:
+			raise Exception('State not found: ', state)
 		return action
+
+	def get_sa(self, state, action):
+		for sa in self.__table:
+			if sa.state == state and sa.action == action:
+				return sa
+		raise Exception('(s,a) pair not found in table: ', state, action)
+
 	def get_q(self, state, action):
 		for sa in self.__table:
 			if sa.state == state and sa.action == action:
 				return sa.q
-		raise NotFoundErr('(s,a) pair not found in table')
-	def set_q(self, state, action, q):
-		for sa in self.__table:
-			if sa.state == state and sa.action == action:
-				sa.q = q
-				return
-		raise NotFoundErr('(s,a) pair not found in table')
+		raise Exception('(s,a) pair not found in table: ', state, action)
 
 class QLearner:
 	def __init__(self, 
@@ -117,37 +95,51 @@ class QLearner:
     discount_factor, 
     episodes,
 	initial_epsilon,
-	final_epsilon
+	final_epsilon,
+	environment
     ):
 		self.__observation_type = observation_type
 		self.__action_type = action_type
 		self.__alpha = learning_rate
 		self.__gamma = discount_factor
 		self.__episodes = episodes
+		self.__episodes_passed = 0
 		self.__initial_epsilon = initial_epsilon
 		self.__epsilon = self.__initial_epsilon
 		self.__final_epsilon = final_epsilon
 		self.__action_space = list(Actions)
-		self.__qtable = QTable()
-		
-		if self.__observation_type == "simple":
-			self.__cur_state, self.__next_state = StateSimple(), StateSimple()
-		else:
-			self.__cur_state, self.__next_state = StateComplex(), StateComplex()
+		self.__environment = environment
+		self.__qtable = QTable(self.__environment.w, self.__environment.h, self.__alpha, self.__gamma)
+		self.__cur_state, self.__next_state = [], []
 
-	def decide(self, observation):
-		self.__cur_state(observation)
-		x, y, xg, yg, los = observation
+	def decide(self, state):
 		if random() > self.__epsilon:
-			return self.__qtable.get_greedy_action(self.__cur_state(x,y,xg,yg))
+			print('GREEDY ', self.__epsilon)
+			return self.__qtable.get_greedy_action(state)
 		else:
+			print('RANDOM', self.__epsilon)
 			return choice(self.__action_space)
+
 	# q(s,a) = q(s,a) + alpha * (r + gamma * max_q(s',a') - q(s,a))
-	def learn(self, cur_obs, next_obs, action, reward, terminal):
-		cur_state, next_state = self.__cur_state.get(), self.__next_state(next_obs)
-		print('s,a', cur_state, action)
-		q = self.__qtable.get_q(cur_state, action)
-		greedy_action = self.__qtable.get_greedy_action(next_state)
-		max_q = self.__qtable.get_q(next_state, greedy_action)
-		q = q + self.__alpha * (reward + self.__gamma * max_q - q)
-		self.__qtable.set_q(cur_state, action, q)
+	def learn(self, cur_state, next_state, action, reward, terminal, neighbour):
+		self.__cur_state, self.__next_state = cur_state, next_state
+		if terminal:
+			self.__episodes_passed += 1
+			self.__epsilon = max(self.__final_epsilon, 
+				self.__episodes_passed * (self.__final_epsilon - self.__initial_epsilon) / self.__episodes + self.__initial_epsilon)
+		
+		max_q = self.__qtable.get_q(next_state, self.__qtable.get_greedy_action(next_state))
+		sa = self.__qtable.get_sa(cur_state, action)
+		sa.update_q(reward, max_q, terminal)
+
+		#print('s,a,ns,r,q: ', cur_state, action, next_state,reward, sa.q)
+		#print('num_visits: ', sa.num_visits)
+
+	def print_result(self):
+		visited_sa_pairs = 0
+		for sa in self.__qtable.table:
+			if sa.visited:
+				visited_sa_pairs += 1
+		print('Visited sa pairs: ', visited_sa_pairs, '/', len(self.__qtable.table))
+		print('Visited percentage (%): ', 100 * visited_sa_pairs / len(self.__qtable.table))
+
